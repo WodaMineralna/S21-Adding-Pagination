f79f055c209b95db5b8ab2690dd7c6d79ec2cf01
WodaMineralna <raknieborak65@gmail.com>
2025-10-27 23:03:17 +0100

    chore(env): change `PRODUCTS_PER_PAGE` from 4 to 3

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ import { fillInvoicePDF } from "../utils/index.js";
 import Product from "../models/product.js";
 
 const PLACEHOLDER_DETAILS = { cause: null, message: "Something went wrong..." };
-const PRODUCTS_PER_PAGE = process.env.PRODUCTS_PER_PAGE || 4;
+const PRODUCTS_PER_PAGE = process.env.PRODUCTS_PER_PAGE || 3;
 
 export async function getIndex(req, res, next) {
   const page = req.query.page;
